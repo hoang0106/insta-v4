6336231f75005d6861802b19cf9ca78da2f5fae4
hoang0106 <nguyen.d.hoang@gmail.com>
2023-08-23 22:05:55 -0700

    add the menu and profile image at the right side of the header

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,11 @@
 import Image from 'next/image';
 import React from 'react';
-import { SearchIcon } from '@heroicons/react/outline';
+import { SearchIcon, PlusCircleIcon } from '@heroicons/react/outline';
+import { HomeIcon } from '@heroicons/react/solid';
 
 export default function Header() {
 	return (
-		<div className='flex items-center justify-between max-w-6xl'>
+		<div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
 			{/* Left */}
 
 			<div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
@@ -24,16 +25,24 @@ export default function Header() {
 
 			{/* Middle */}
 
-			<div className='relative mt-1'>
-				<div className='absolute top-2 left-2'>
-					<SearchIcon  className='h-5 text-gray-500'/>
+			<div className="relative mt-1">
+				<div className="absolute top-2 left-2">
+					<SearchIcon className="h-5 text-gray-500" />
 				</div>
-				<input type="text" placeholder='Search' className='bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md'/>
+				<input
+					type="text"
+					placeholder="Search"
+					className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
+				/>
 			</div>
 
 			{/* Right */}
 
-			<h1>Right Side</h1>
+			<div className='flex space-x-4 items-center'>
+				<HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 translate-transform duration-200 ease-out" />
+				<PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 translate-transform duration-200 ease-out" />
+				<img src='American-West.JPG' alt='user-image' className='h-6 w-6 rounded-full cursor-pointer' />
+			</div>
 		</div>
 	);
 }
