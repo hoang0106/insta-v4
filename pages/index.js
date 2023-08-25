91ef5b78c91297b9ffb5db8fe4f57b2c8f95f671
hoang0106 <nguyen.d.hoang@gmail.com>
2023-08-25 16:04:42 -0700

    populate fake data using minifaker and map through it inside story component and create feed and story component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Header from '@/components/Header';
+import Feed from '@/components/Feed';
 
 export default function Home() {
 	return (
@@ -15,6 +16,8 @@ export default function Home() {
 			<Header />
 
 			{/* Feed */}
+
+			<Feed />
 			
 			{/* Modal */}
 		</div>
