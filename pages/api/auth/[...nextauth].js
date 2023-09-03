2935679b1f76111400490e3abf37b5a1377dace8
hoang0106 <nguyen.d.hoang@gmail.com>
2023-09-02 21:19:01 -0700

    complete the signin page

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,19 +1,28 @@
-import NextAuth from "next-auth"
-import GoogProvider from "next-auth/providers/google"
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
 
-export const authOptions = {
-  // Configure one or more authentication providers
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-    // ...add more providers here
-  ],
+export default NextAuth({
+	// Configure one or more authentication providers
+	providers: [
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+		}),
+		// ...add more providers here
+	],
+	// secret: process.env.SECRET,
+	pages: {
+		signIn: '/auth/signin',
+	},
 
-  pages: {
-    signin: "auth/signin",
-  }
-}
-
-export default NextAuth(authOptions)
+	callbacks: {
+		async session({ session, token, user }) {
+			session.user.username = session.user.name
+				.split(' ')
+				.join('')
+				.toLocaleLowerCase();
+			session.user.uid = token.sub;
+			return session;
+		},
+	},
+});
--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,19 +1,37 @@
 import { getProviders, signIn } from 'next-auth/react';
 import Header from '@/components/Header';
+import React from 'react';
 
 export default function signin({ providers }) {
 	return (
 		<>
 			<Header />
-			<div>
+			<div className="flex justify-center space-x-7 mt-20">
 				<img
+					className="hidden object-cover rotate-6 md:inline-flex md:w-48"
 					src="https://superviral.com.au/wp-content/uploads/2021/08/instagix-banner-graphic.png"
 					alt="instagram-image"
 				/>
 				<div>
 					{Object.values(providers).map((provider) => (
-						<div>
-							<img src="https://socodigital.com/wp-content/uploads/2021/03/Instagram.png" />
+						<div key={provider.name} className="flex flex-col items-center">
+							<img
+								className="w-32 object-cover"
+								src="https://socodigital.com/wp-content/uploads/2021/03/Instagram.png"
+							/>
+							<p className="text-sm italic my-10 text-center">
+								This app is created for learning purposes
+							</p>
+							<button
+								onClick={() =>
+									signIn(provider.id, {
+										callbackUrl: '/',
+									})
+								}
+								className="bg-red-400 rounded-lg p-3 text-white  hover:bg-red-500"
+							>
+								Sign in with {provider.name}
+							</button>
 						</div>
 					))}
 				</div>
